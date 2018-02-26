b0d5a6877e29a58489581cdc4216720e7cbaef85
劉嘉祐 <ks860108@gmail.com>
2018-02-26 22:44:15 +0800

    PHP

--- test1.php
+++ test1.php
@@ -1,5 +1,19 @@
-﻿<?php
+﻿<!DOCTYPE html> 
+<html> 
+<body> 
 
-echo "";
+<?php 
+include("test.php");
+			
+			$sql = "SELECT * FROM led";
+			if($stmt = $db->query($sql))
+		{
+			while($result = mysqli_fetch_object($stmt))
+				{
+					echo '<p>名字： '.$result->ID.'，學號：'.$result->light1.'</p>';
+				}
+			}
+		?>
 
-?>
+</body> 
+</html>
