22cb6f2288db5b688c820d5f49ec18fcc175d31f
劉嘉祐 <ks860108@gmail.com>
2018-02-28 00:53:53 +0800

    PHP

--- test1.php
+++ test1.php
@@ -2,18 +2,33 @@
 <html> 
 <body> 
 
-<?php 
-include("test.php");
-			
-			$sql = "SELECT * FROM led";
-			if($stmt = $db->query($sql))
+<?php
+$conn = mysqli_connect("120.119.72.53:8609","root","123456");
+
+if (!$conn){
+  die("Could not connect: " . mysqli_connect_error($conn));
+}
+
+echo "connect successfully";
+
+
+$sql = "SELECT ID, light1, light2, light3, light4, light5, light6, light7, light8 FROM led";
+
+if ($result=mysqli_query($conn,$sql))
+	
+	{
+		while ($row = mysqli_fetch_row($result))
 		{
-			while($result = mysqli_fetch_object($stmt))
-				{
-					echo '<p>名字： '.$result->ID.'，學號：'.$result->light1.'</p>';
-				}
-			}
-		?>
+			printf ("%s  : %s ",$row[0],$row[1]);
+			echo "<br>";
+		}
+		
+		mysqli_free_result($result);
+	}
+	
+
+mysqli_close($conn);
+?>
 
 </body> 
 </html>
