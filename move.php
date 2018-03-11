f3c180fda385b2ffc1dcbafd4ec87c688cd7635f
劉嘉祐 <ks860108@gmail.com>
2018-03-11 16:09:51 +0800

    3/11

---
+++ move.php
@@ -0,0 +1,326 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<!-- <meta http-equiv="refresh" content="9"> -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<style>
+
+	.arialFont{
+		font-family:arial;
+	}
+	.centerButton{
+		width:150px;
+		height:60px;
+		font-size:25px
+	}
+		
+<title>動態網頁</title>
+</style></head>
+
+<body>
+	<!--先設立每個檔案的ID-->
+	<div class ="container">
+		<div class = "text-center">
+			<h3 class="arialFont" id="slave">Slave：</h3>
+			<h3 class="arialFont" id="program">Program：</h3>
+			<h3 class="arialFont" id="delay">Delay：</h3>
+			<id = "monitor">
+		</div>
+	</div>		
+	<hr size="20" />
+	
+	<div class ="container">
+		<div class="row col-md-12 text-center" id="result"></div>
+	</div>	
+	
+	<div class="container">
+  			<div class = "text-center">
+			<a href = "http://120.119.72.53:8080/home.html" class = "btn btn-info btn-lg centerButton">回到上頁</a>
+	</div>
+	
+	
+	
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script>
+		$.ajax({
+			url: 'http://120.119.72.53:8080/monitor_slave_json.php',
+			dataType: "json",
+			type: 'post',
+			async: false,
+			error: function(xhr) {
+			  alert('Ajax request 發生錯誤');
+			},
+			success: function(response) {
+				resSlave = response;
+				if(resSlave[4] == "1"){
+					$("#slave").html('Slave：'+resSlave[1]);
+					$("#program").html('Program：'+resSlave[2]);
+					$("#delay").html('Delay：'+resSlave[3]+ 'S');
+				}else{
+					$("#slave").html('Slave：wait');
+					$("#program").html('Program：');
+					$("#delay").html('Delay：');
+				}
+				
+				if(resSlave[1]!=""){
+					$("#slave").html('Slave：'+resSlave[1]);
+					$("#program").html('Program：'+resSlave[2]);
+					$("#delay").html('Delay：'+resSlave[3]+ 'S');
+					
+				}else{
+					$("#slave").html('Slave：wait');
+					$("#program").html('Program：wait');
+					$("#delay").html('Delay：wait');
+				}
+			}
+		});
+
+		$.ajax({
+			url: 'http://120.119.72.53:8080/monitor_light_json.php',
+			dataType: "json",
+			type: 'post',
+			async: false,
+			error: function(xhr) {
+			  alert('Ajax request 發生錯誤');
+			},
+			success: function(response) {
+				resLight = response;
+			}
+		});
+		
+		timerSlave = "";
+		timerLight = "";
+		
+		resArray();
+		firstChange();
+		changeLightS(1);
+/*		var changelight = resLight;
+		if(changelight === resLight){
+		break;} else if(changelight != resLight){
+			
+		}	*/
+		
+		//十轉二進制,往前補0補到8bit
+		function resArray(){
+				{	
+			{var x1 = parseInt(resLight[1]);
+				var showX1 = x1.toString(2);	
+				function padLeft(showX1){
+				if(showX1.length>=8)	return showX1;
+				else	return padLeft("0"+showX1);
+			}}
+
+			{var x2 = parseInt(resLight[2]);
+			var showX2 = x2.toString(2);
+			
+			function padLeft(showX2){
+				if(showX2.length>=8)	return showX2;
+				else	return padLeft("0"+showX2);
+			}}
+
+			{var x3 = parseInt(resLight[3]);
+			var showX3 = x3.toString(2);
+			
+			function padLeft(showX3){
+				if(showX3.length>=3)	return showX3;
+				else	return padLeft("0"+showX3);
+		}}
+			
+			{var x4 = parseInt(resLight[4]);
+			var showX4 = x4.toString(2);
+			
+			function padLeft(showX4){
+				if(showX4.length>=8)	return showX4;
+				else	return padLeft("0"+showX4);
+			}}
+			
+			{var x5 = parseInt(resLight[5]);
+			var showX5 = x5.toString(2);
+			
+			function padLeft(showX5){
+				if(showX5.length>=8)	return showX5;
+				else	return padLeft("0"+showX5);
+			}}
+			
+			{var x6 = parseInt(resLight[6]);
+			var showX6 = x6.toString(2);
+			
+			function padLeft(showX6){
+				if(showX6.length>=8)	return showX6;
+				else	return padLeft("0"+showX6);
+			}}
+			
+			{var x7 = parseInt(resLight[7]);
+			var showX7 = x7.toString(2);
+			
+			function padLeft(showX7){
+				if(showX7.length>=8)	return showX7;
+				else	return padLeft("0"+showX7);
+			}}
+			
+			{var x8 = parseInt(resLight[8]);
+			var showX8 = x8.toString(2);
+			
+			function padLeft(showX8){
+				if(showX8.length>=8)	return showX8;
+				else	return padLeft("0"+showX8);
+			}}	
+				
+			
+			{//將數字分割轉換回陣列
+			newArray1 = new Array();
+			
+			newArray1 = padLeft(showX1).split("");
+			
+			newArray2 = new Array();
+			newArray2 = padLeft(showX2).split("");
+			
+			
+			newArray3 = new Array();
+			newArray3 = padLeft(showX3).split("");
+			
+			newArray4 = new Array();
+			newArray4 = padLeft(showX4).split("");
+
+			newArray5 = new Array();
+			newArray5 = padLeft(showX5).split("");
+			
+			newArray6 = new Array();
+			newArray6 = padLeft(showX6).split("");
+			
+			newArray7 = new Array();
+			newArray7 = padLeft(showX7).split("");
+
+			newArray8 = new Array();
+			newArray8 = padLeft(showX8).split("");
+				}
+			}
+		}
+		
+		function firstChange(){
+			$("#result").html('');
+			$("#result").append("<tr>");
+			for(i=0;i<8;i++){
+				if(newArray1[i]=="1"){
+				$("#result").append('<img src="https://tse2.mm.bing.net/th?id=OIP.R7ebTKLI1dEK5uWLzHxJrwHaHa&amp" style="width:10%;height:13%"/>');
+				}else{
+				$("#result").append('<img src="https://tse2.mm.bing.net/th?id=OIP.wtAXd9C6IMtdzCGQMcT89QHaHa&amp" style="width:10%;height:13%"/>');	
+				}		
+			}
+			$("#result").append("</tr>");	
+		}
+
+	//設定Slave,Program,Delay以秒一次
+	timerSlave = setInterval(
+			function(){
+				$.ajax({
+			url: 'http://120.119.72.53:8080/monitor_slave_json.php',
+			dataType: "json",
+			type: 'post',
+			async: false,
+			error: function(xhr) {
+			  alert('Ajax request 發生錯誤');
+			},
+			success: function(response) {
+				resSlave = response;
+			}
+		});
+		//如果每500毫秒取得的值為空值，則不更新頁面內容
+				if(resSlave[4] == "1"){
+					if(resSlave[1]!=""){
+						$("#slave").html('Slave：'+resSlave[1]);
+						$("#program").html('Program：'+resSlave[2]);
+						$("#delay").html('Delay：'+resSlave[3]+ 'S');
+						//取得燈號資訊
+						$.ajax({
+							url: 'http://120.119.72.53:8080/monitor_light_json.php',
+							dataType: "json",
+							type: 'post',
+							async: false,
+							error: function(xhr) {
+							  alert('Ajax request 發生錯誤');
+							},
+							success: function(response) {
+								//無法取得燈號則不動作
+								if(response[0]!="false"){
+									//取得的燈號與現在的燈號不相等才執行更換燈號的動作
+									if(response.sort().toString() != resLight.sort().toString()){
+										resLight = response;
+										resArray();
+										changeLightS(1);
+									}
+								}
+							}
+						});
+					}
+				}else if(resSlave[4] =="0"){
+					$("#slave").html('Slave：wait');
+					$("#delay").html('Delay：');
+					$("#result").html('');
+					if(resSlave[2] == "0"){
+					$("#program").html('Program：wait');	
+					$("#result").html('');
+				}
+				}
+	
+			},500
+		);
+
+		
+		
+/*	timerFlash = setInterval(
+		function(){
+			$("#result").html('');
+			$.ajax({
+			url: 'http://120.119.72.53:8080/monitor_light_json.php',
+			dataType: "json",
+			type: 'post',
+			async: false,
+			error: function(xhr) {
+			  alert('Ajax request 發生錯誤');
+			},
+			success: function(response) {
+				resLight = response;
+			}
+		});
+				$("#result").append(resLight);
+		},1000
+		);
+*/
+		
+	function changeLightS(num){
+		//清除原先的指令
+		clearInterval(timerLight);
+		changeNum=num;
+		timerLight = setInterval(
+			function(){
+				$("#result").html('');
+				$("#result").append("<tr>");
+				for(i=0;i<8;i++){
+				if(eval("newArray"+changeNum+"[i]=='1'")){
+					$("#result").append('<img src="https://tse2.mm.bing.net/th?id=OIP.R7ebTKLI1dEK5uWLzHxJrwHaHa&amp" style="width:10%;height:13%"/>');
+					}else{
+					$("#result").append('<img src="https://tse2.mm.bing.net/th?id=OIP.wtAXd9C6IMtdzCGQMcT89QHaHa&amp" style="width:10%;height:13%"/>');	
+					}		
+				}
+				
+				$("#result").append("</tr>");
+				changeNum = changeNum==8?1:changeNum+1;
+			},(resSlave[3]*1000)
+		);
+	}
+	
+
+	
+				
+		<!-- 將 select 的值在印出 -->
+		//var num = document.getElementById("number").value;
+		//$("#result").html('');
+		//$("#result").append(res[num][0]);    
+	</script>
+
+</body>
+
+</html>
--- static.php
+++ static.php
@@ -1,35 +1,45 @@
-﻿
+
 <html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-
-  <title>get select value</title>
+	<style>
+	
+	.centerButton{
+		width:150px;
+		height:60px;
+		font-size:25px
+	}
+	</style>
+  <title>靜態網頁</title>
 </head>
 
 <body>
-
+	
 	<div class = "container">
-		<div class ="text-center">
-			
-<select name="number" id="number" onchange="print_value();">
-<option value="">請選取</option>
-</select>
- 
+		<div class="col-md-4"></div>
+		<div class ="col-md-4  text-center">
+			<select name="number" id="number" onchange="print_value();" class=" form-control">
+				<option value="">請選取</option>
+			</select>
+			<br>
 		</div>
+		<div class="row col-md-4"></div>
+		<!-- 印出結果 -->
+		<div class="row col-md-12 text-center" id="result"></div>
 	</div>
  
-<!-- 印出結果 -->
-<div id="result">
-	
-</div>
+	<div class="container">
+  			<div class = "text-center">
+			<a href = "http://120.119.72.53:8080/home.html" class = "btn btn-info btn-lg centerButton">回到上頁</a>
+	</div>
  
  <script src="js/jquery-3.3.1.min.js"></script>
 <script>
 
 	$.ajax({
-    url: 'http://120.119.72.53:8080/static.php',
+    url: 'http://120.119.72.53:8080/static_json.php',
 	dataType: "json",
     type: 'post',
 	async: false,
@@ -44,30 +54,149 @@
 		res = response;
     }
   });
-  
-  
 
-	
   
- function print_value() {
+  
+ timer="";
+function print_value() {
 	<!-- 將 select 的值在印出 -->
+	clearInterval(timer);
 	var num = document.getElementById("number").value;
 	$("#result").html('');
 	$("#result").append("<tr>");
-	for(i=1;i<=8;i++){
-		if(res[num][i]=="1"){
-			$("#result").append('<img src="https://tse2.mm.bing.net/th?id=OIP.R7ebTKLI1dEK5uWLzHxJrwHaHa&amp" width="105" height="100"/>');
+	
+	
+	{//parseInt將字串轉為數字,再將數字轉為2進制,在利用function將未滿8bit數字想左補0補滿到8bit
+	
+	{var x1 = parseInt(res[num][1]);
+	var showX1 = x1.toString(2);	
+	function padLeft(showX1){
+		if(showX1.length>=8)	return showX1;
+		else	return padLeft("0"+showX1);
+	}}
+
+	{var x2 = parseInt(res[num][2]);
+	var showX2 = x2.toString(2);
+	
+	function padLeft(showX2){
+		if(showX2.length>=8)	return showX2;
+		else	return padLeft("0"+showX2);
+	}}
+
+	{var x3 = parseInt(res[num][3]);
+	var showX3 = x3.toString(2);
+	
+	function padLeft(showX3){
+		if(showX3.length>=3)	return showX3;
+		else	return padLeft("0"+showX3);
+}}
+	
+	{var x4 = parseInt(res[num][4]);
+	var showX4 = x4.toString(2);
+	
+	function padLeft(showX4){
+		if(showX4.length>=8)	return showX4;
+		else	return padLeft("0"+showX4);
+	}}
+	
+	{var x5 = parseInt(res[num][5]);
+	var showX5 = x5.toString(2);
+	
+	function padLeft(showX5){
+		if(showX5.length>=8)	return showX5;
+		else	return padLeft("0"+showX5);
+	}}
+	
+	{var x6 = parseInt(res[num][6]);
+	var showX6 = x6.toString(2);
+	
+	function padLeft(showX6){
+		if(showX6.length>=8)	return showX6;
+		else	return padLeft("0"+showX6);
+	}}
+	
+	{var x7 = parseInt(res[num][7]);
+	var showX7 = x7.toString(2);
+	
+	function padLeft(showX7){
+		if(showX7.length>=8)	return showX7;
+		else	return padLeft("0"+showX7);
+	}}
+	
+	{var x8 = parseInt(res[num][8]);
+	var showX8 = x8.toString(2);
+	
+	function padLeft(showX8){
+		if(showX8.length>=8)	return showX8;
+		else	return padLeft("0"+showX8);
+	}}	
+	}	
+	
+	//將數字分割轉換回陣列
+	var newArray1 = new Array();
+	
+	var newArray1 = padLeft(showX1).split("");
+
+	var newArray2 = new Array();
+	var newArray2 = padLeft(showX2).split("");
+	
+	var newArray3 = new Array();
+	var newArray3 = padLeft(showX3).split("");
+	
+	var newArray4 = new Array();
+	var newArray4 = padLeft(showX4).split("");
+	
+	var newArray5 = new Array();
+	var newArray5 = padLeft(showX5).split("");
+	
+	var newArray6 = new Array();
+	var newArray6 = padLeft(showX6).split("");
+	
+	var newArray7 = new Array();
+	var newArray7 = padLeft(showX7).split("");
+
+	var newArray8 = new Array();
+	var newArray8 = padLeft(showX8).split("");
+
+	
+	
+	//顯示出light1節目之亮法	
+	for(i=0;i<8;i++){
+		if(newArray1[i]=="1"){
+		$("#result").append('<img src="https://tse2.mm.bing.net/th?id=OIP.R7ebTKLI1dEK5uWLzHxJrwHaHa&amp" style="width:10%;height:13%"/>');
 		}else{
-			$("#result").append('<img src="https://tse2.mm.bing.net/th?id=OIP.wtAXd9C6IMtdzCGQMcT89QHaHa&amp" width="105" height="105"/>');
-		}
+		$("#result").append('<img src="https://tse2.mm.bing.net/th?id=OIP.wtAXd9C6IMtdzCGQMcT89QHaHa&amp" style="width:10%;height:13%"/>');	
+		}		
 	}
 	$("#result").append("</tr>");
-	//$("#result").append(res[num][1]+res[num][2]+res[num][3]+res[num][4]+res[num][5]+res[num][6]+res[num][7]+res[num][8]);
-	
-}	
- 
-</script>
+	//setInterval(alert(),1000);
 
+	changeNum=2;
+	timer = setInterval(
+		function(){
+			$("#result").html('');
+			$("#result").append("<tr>");
+			for(i=0;i<8;i++){
+			if(eval("newArray"+changeNum+"[i]=='1'")){
+				$("#result").append('<img src="https://tse2.mm.bing.net/th?id=OIP.R7ebTKLI1dEK5uWLzHxJrwHaHa&amp" style="width:10%;height:13%"/>');
+				}else{
+				$("#result").append('<img src="https://tse2.mm.bing.net/th?id=OIP.wtAXd9C6IMtdzCGQMcT89QHaHa&amp" style="width:10%;height:13%"/>');	
+				}		
+			}
+			$("#result").append("</tr>");
+			changeNum = changeNum==8?1:changeNum+1;
+		},1000
+	);
+	//setTimeout("change(2)", 1000);
+	//change(2);
+	
+	
+
+	function myFunction(){
+		document.getElementById("result").innerHTML= newArray2;
+	}
+}
+</script>
  
 </body>
 </html>
